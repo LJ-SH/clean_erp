b88f4d16f2a220630d51447d6e039b5b82577a69
IloveRuby <ljun1999@hotmail.com>
2013-04-07 15:31:52 +0800

    1st release at 7-Apr-2013

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CleanErp::Application.config.secret_token = '719388ba0f79ce2f86961afce87ae5196c37bb1e4906f02a8acd8a1e4c2a41d298491e25c16e3df665cbbe66c9152090b16cb2bf7232c22cad183bb7ab59ec13'
